920b3a04ae312ad71b179fb7a17c9e30f53cc3f5
Ekimaru <74165503+Ekimaru@users.noreply.github.com>
2021-03-19 12:35:45 -0500

    delete unused image example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'; 
-  
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 // Css
 import './App.css'
 
@@ -17,29 +17,28 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import PininaDashboard from './pages/PininaDashboard'
 import SaciDashboard from './pages/SaciDashboard'
-import ImagesExample from './pages/ImagesExample'
 
 
 // import classes from '*.module.css';
 
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
+  palette: {
+    primary: {
 
-            main: '#63c132',
-            // main: '#432918'
-            // main: '#E4DAD8',
+      main: '#63c132',
+      // main: '#432918'
+      // main: '#E4DAD8',
 
-        },
-        secondary: {
-            main: '#1E3F20'
-
-        },
     },
-    typography: {
-        fontFamily: `'Poppins', 'sans-serif'`,
-    }
+    secondary: {
+      main: '#1E3F20'
+
+    },
+  },
+  typography: {
+    fontFamily: `'Poppins', 'sans-serif'`,
+  }
 })
 
 
@@ -47,22 +46,21 @@ const theme = createMuiTheme({
 function App() {
 
   return (
-    <Provider store={ store }>
+    <Provider store={store}>
       <ThemeProvider theme={theme}>
         <Router>
           <Switch>
-            <Route exact path="/" component={SaciDashboard}/>
-            <Route path="/Register" component={Register}/>
-            <Route path="/Login" component={Login}/>
-            <Route path="/Pinina" component={PininaDashboard}/>
-            <Route path="/images" component={ImagesExample}/>
+            <Route exact path="/" component={SaciDashboard} />
+            <Route path="/Register" component={Register} />
+            <Route path="/Login" component={Login} />
+            <Route path="/Pinina" component={PininaDashboard} />
             {/* <PrivateRoute path="/menu-user" component={MenuUser} /> */}
-            
+
           </Switch>
         </Router>
       </ThemeProvider>
 
-    
+
     </Provider>
   );
 }
